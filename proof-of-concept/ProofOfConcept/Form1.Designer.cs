b30069df136a2e88b2e30435fc17c7e8533fda56
Siarhei Kuchuk <Cuchuk.Sergey@gmail.com>
2019-04-23 20:12:07 +0300

    proof of concept

---
+++ proof-of-concept/ProofOfConcept/Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace ProofOfConcept
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(605, 255);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(333, 120);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Send message";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(49, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "MY ID";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(49, 162);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "MY PWD";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(481, 41);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(74, 17);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Remote ID";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(481, 162);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(93, 17);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Remote PWD";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(127, 38);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(273, 22);
+            this.textBox1.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(127, 157);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(273, 22);
+            this.textBox2.TabIndex = 6;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(654, 38);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(284, 22);
+            this.textBox3.TabIndex = 7;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(654, 162);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(284, 22);
+            this.textBox4.TabIndex = 8;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(194, 255);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(333, 120);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Get Messages";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(654, 208);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(284, 22);
+            this.textBox5.TabIndex = 11;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(481, 208);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(65, 17);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Message";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(989, 417);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ proof-of-concept/ProofOfConcept/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Script.Serialization;
+using System.Windows.Forms;
+
+namespace ProofOfConcept
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private Server _server = new Server();
+        private MyToken _myToken;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            _myToken = _server.Register();
+            textBox1.Text = _myToken.Id;
+            textBox2.Text = _myToken.Password;
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            var messages = _server.GetMessages(_myToken.Token);
+            foreach (var message in messages)
+            {
+                MessageBox.Show(message.Message);
+            }
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            _server.SendMessage(_myToken.Token, int.Parse(textBox3.Text), textBox4.Text, textBox5.Text);
+        }
+    }
+
+    class Server
+    {
+        private const string _serverUrl = "http://localhost:3000";
+
+
+        public MyToken Register()
+        {
+            using (var wc = new WebClient())
+            {
+                // wc.Headers[HttpRequestHeader.ContentType] = "application/x-www-form-urlencoded";
+                string HtmlResult = wc.UploadString(_serverUrl + "/token", "");
+
+                var json = new JavaScriptSerializer();
+                var data = json.Deserialize<MyToken>(HtmlResult);
+                return data;
+            }
+        }
+
+        public ServerMessage[] GetMessages(string token)
+        {
+            using (var wc = new WebClient())
+            {
+                // wc.Headers[HttpRequestHeader.ContentType] = "application/x-www-form-urlencoded";
+                string HtmlResult = wc.DownloadString(_serverUrl + "/client/"+ token+"/messages");
+
+                var json = new JavaScriptSerializer();
+                var data = json.Deserialize<ServerMessage[]>(HtmlResult);
+                return data;
+            }
+        }
+
+        public void SendMessage(string token, int remoteId, string remotePassword, string message)
+        {
+            using (var wc = new WebClient())
+            {
+                var json = new JavaScriptSerializer();
+                var dataSend = json.Serialize(new
+                {
+                    id = remoteId,
+                    password = remotePassword,
+                    message = message,
+                });
+
+                wc.Headers[HttpRequestHeader.ContentType] = "application/json";
+                string HtmlResult = wc.UploadString(_serverUrl + "/client/"+ token + "/message", dataSend);
+
+            }
+        }
+    }
+
+    class ServerMessage
+    {
+        public string FromToken { get; set; }
+        public string ToToken { get; set; }
+        public string Message { get; set; }
+    }
+
+    class MyToken
+    {
+        public string Id { get; set; }
+        public string Password { get; set; }
+        public string Token { get; set; }
+    }
+}
